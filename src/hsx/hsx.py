b210e3d94b638004b822a9c9a5ed2abf8966761e
Michael Sparks <sparks.m@gmail.com>
2026-03-24 15:54:33 +0000

    Placeholder code

---
+++ src/hsx/hsx.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+# Version 0 has no (useful) code to allow packaging to be set up correctly
+
+def about():
+    print("This is the version 0.0.0 of the hsx library and tool")
+
+def main():
+    about()
+
+if __name__ == "__main__":
+    main()
